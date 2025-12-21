91bd59f2afd7f5d5690aa31735f859f5c5775050
Matthew Wijaya <matthew.wijaya@ui.ac.id>
2025-12-21 23:18:15 +0700

    Trial Fix Google AGAIN

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,17 +4,16 @@ import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
+val hasKeystore = keystorePropertiesFile.exists();
+
+if (hasKeystore) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
-
-
 android {
     namespace = "com.askmo.mobile"
     compileSdk = flutter.compileSdkVersion
@@ -31,8 +30,8 @@ android {
 
     defaultConfig {
         applicationId = "com.askmo.mobile"
-        minSdk = 24
-        targetSdk = 36
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -50,7 +49,7 @@ android {
 
 buildTypes {
     release {
-        signingConfig = if (keystorePropertiesFile.exists())
+        signingConfig = if (hasKeystore)
             signingConfigs.getByName("release")
         else
             signingConfigs.getByName("debug")
